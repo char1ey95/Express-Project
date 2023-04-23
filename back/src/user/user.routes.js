de47777d01d635fbd1ad6df8c93ba5af6c48fd97
root <root@Char1ey>
2023-04-24 02:02:30 +0900

    fixed: user 라우터 정리

--- back/src/user/user.routes.js
+++ back/src/user/user.routes.js
@@ -4,13 +4,11 @@ const router = express.Router()
 
 
 router.get('/join', (req, res, next) => userController.getJoin(req, res, next))
-
 router.post('/join', (req, res, next) => userController.postJoin(req, res, next))
 
 router.get('/welcome', (req, res, next) => userController.getWelcome(req, res, next))
 
 router.get('/login', (req, res, next) => userController.getLogin(req, res, next))
-
 router.post('/login', (req, res, next) => userController.postLogin(req, res, next))
 
 router.get('/logout', (req, res, next) => userController.getLogout(req, res, next))
